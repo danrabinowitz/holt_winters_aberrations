c4ec744bd6f207dc2f2b64406b9cd2342e89bf57
Dan Rabinowitz <djr@DanielRabinowitz.com>
2016-02-24 23:44:50 -0700

    Add test for initial seasonal

--- spec/holt_winters/multiplicative_forecaster_spec.rb
+++ spec/holt_winters/multiplicative_forecaster_spec.rb
@@ -26,7 +26,22 @@ describe HoltWinters::MultiplicativeForecaster do
         f = HoltWinters::MultiplicativeForecaster.new(data: data, α: 0.05, s: 12)
         warn "WARNING: This matches the Excel data, but not the formula I'm using."
         expect(f.trend_s).to eq(6.to_r/11)
-      end      
+      end
+    end
+  end
+
+  describe "seasonal" do
+    describe "initial values" do
+      describe "known data" do
+        it "returns the known values" do
+          data = [112,118,132,129,121,135,148,148,136,119,104,118]
+          f = HoltWinters::MultiplicativeForecaster.new(data: data, α: 0.05, s: 12)
+          warn "WARNING: This matches the Excel data, but not the formula I'm using."
+          expect(f.seasonal(0)).to eq(84/95.to_r)
+          expected_data = [0.88,0.93,1.04,1.02,0.96,1.07,1.17,1.17,1.07,0.94,0.82,0.93]
+          expect((0..11).to_a.map{|i| f.seasonal(i).round(2) }).to eq(expected_data)
+        end
+      end
     end
   end
 
